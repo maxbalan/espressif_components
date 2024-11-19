7a95f12257be241a7a314ede518d9573ebec31be
max <balan.maxim@gmail.com>
2024-11-19 19:03:51 +0000

    add retry to wifi library for disconnect events

--- components/wifi_helper/WifiHelper.c
+++ components/wifi_helper/WifiHelper.c
@@ -16,13 +16,16 @@ static void wifi_event_handler(void *arg,
                                esp_event_base_t event_base,
                                int32_t event_id,
                                void *event_data) {
-   if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
         esp_wifi_connect();
         ESP_LOGI(TAG, "Station started");
     } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
         ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
         ESP_LOGI(TAG, "Got IP:" IPSTR, IP2STR(&event->ip_info.ip));
         xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        ESP_LOGW(TAG, "Disconnected from Wi-Fi, retrying...");
+        esp_wifi_connect();
     }
 }
 
@@ -54,10 +57,8 @@ void connectWifi(wifi_config_t wifiConfig) {
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifiConfig));
 
-
     // connect WIFI
     ESP_ERROR_CHECK(esp_wifi_start());
-
 
     EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
                                            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
